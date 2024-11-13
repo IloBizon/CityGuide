ad5c8b413b1581b24f9107a2842cfb699be16acf
Тимофей Гукасов <TiGukasov@alabuga.local>
2024-11-13 11:35:35 +0300

    Добавлена генерация пагинации с помощью js

--- assets/js/pagination.js
+++ assets/js/pagination.js
@@ -3,7 +3,8 @@ const maxCards = 2
 const prevButton = document.getElementById('pagination_prev'); 
 const nextButton = document.getElementById('pagination_next');
 const pagination = document.getElementById("pagination")
-const pageLinks = document.querySelectorAll('.pagination__page'); 
+const pageLinks = document.getElementById('pag_links');
+let links = []
 
 let totalPages = Math.ceil(pag_cards.length / maxCards); 
 let currentPage = 1; 
@@ -28,13 +29,42 @@ export function displayPage(page) {
     }); 
 } 
 
+export function generatePagination(){
+    pag_cards = document.querySelectorAll('.products__card');
+    totalPages = Math.ceil(pag_cards.length / maxCards);
+    console.log(totalPages)
+    for (let index = 0; index < totalPages; index++) {
+        let a = document.createElement("a")
+        a.href = "#"
+        a.className = "pagination__page"
+        a.setAttribute("data-page", index + 1)
+        a.innerHTML = index + 1
+        pageLinks.appendChild(a)
+
+        a.addEventListener('click', (e) => { 
+            e.preventDefault(); 
+            const page = parseInt(a.getAttribute('data-page')); 
+            if (page !== currentPage) { 
+                currentPage = page; 
+                displayPage(currentPage); 
+                updatePagination(); 
+            } 
+        }); 
+        links.push(a)
+    }
+}
+
 export function updatePagination() {
     pag_cards = document.querySelectorAll('.products__card');
     
     totalPages = Math.ceil(pag_cards.length / maxCards);
     prevButton.disabled = currentPage === 1; 
     nextButton.disabled = currentPage === totalPages; 
-    pageLinks.forEach((link) => {
+
+
+
+
+    links.forEach((link) => {
         const page = parseInt(link.getAttribute('data-page')); 
         link.classList.toggle('active', page === currentPage);
         
@@ -53,7 +83,7 @@ export function updatePagination() {
     else {
         pagination.style.display = "flex"
     }
-} 
+}    
 
 
 prevButton.addEventListener('click', (e) => {
@@ -75,18 +105,6 @@ nextButton.addEventListener('click', (e) => {
     } 
 }); 
 
-
-pageLinks.forEach((link) => { 
-    link.addEventListener('click', (e) => { 
-        e.preventDefault(); 
-        const page = parseInt(link.getAttribute('data-page')); 
-        if (page !== currentPage) { 
-            currentPage = page; 
-            displayPage(currentPage); 
-            updatePagination(); 
-        } 
-    }); 
-}); 
 
 
 
